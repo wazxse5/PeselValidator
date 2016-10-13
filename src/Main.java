7be3036b05de749f999bdc2a2db0f1c09d8d7292
wazxse5 <wazxse5@gmail.com>
2016-10-14 00:01:30 +0200

    final version

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
 		while(true) {
 			String pesel = JOptionPane.showInputDialog(null, "Podaj PESEL", "PeselValidator", JOptionPane.QUESTION_MESSAGE);
 			
-			
 			////// wszystko to co zły użytkownik może wpisać źle
 			boolean stringGood = true;
 			if (pesel == null) {
