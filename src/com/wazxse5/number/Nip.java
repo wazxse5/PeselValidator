39f2b31d50e39e4cc73009a751873c788a4af417
wazxse5 <wazxse5@gmail.com>
2018-02-26 22:31:05 +0100

    Names of tax offices. New file that contains names of tax offices and their IDs (first three digits of Nip number is an id of tax office that gave the number).

--- src/com/wazxse5/number/Nip.java
+++ src/com/wazxse5/number/Nip.java
@@ -1,8 +1,24 @@
 package com.wazxse5.number;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Nip extends Number {
     private static final int[] weightTable = {6, 5, 7, 2, 3, 4, 5, 6, 7};
+    private static Properties taxOffices;
 
+    // wczytanie pliku z nazwami urzędów skarbowych
+    static {
+        try {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            InputStream inputStream = loader.getResourceAsStream("NipTaxOffices.properties");
+            taxOffices = new Properties();
+            taxOffices.load(inputStream);
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+            taxOffices = null;
+        }
+    }
 
     /**
      * Podstawowy konstruktor
@@ -47,6 +63,10 @@ public class Nip extends Number {
      */
     @Override
     public String getSimpleInfo() {
-        return "tu pojawią się informacje ...";
+        if (taxOffices != null) {
+            String officeCode = super.getNumber().substring(0, 3);
+            return taxOffices.getProperty(officeCode);
+        } else return "brak informacji";
     }
+
 }
