d2040bd85e84739b75e8a71f6d23be3bead6283a
wazxse5 <wazxse5@gmail.com>
2018-03-05 12:49:53 +0100

    Regon full support.

--- src/com/wazxse5/number/Regon.java
+++ src/com/wazxse5/number/Regon.java
@@ -2,6 +2,7 @@ package com.wazxse5.number;
 
 public class Regon extends Number {
     private static final int[] weightTable9 = {8, 9, 2, 3, 4, 5, 6, 7};
+    private static final int[] weightTable14 = {2, 4, 8, 5, 0, 9, 7, 3, 6, 1, 2, 4, 8};
 
 
     public Regon(String number) {
@@ -28,15 +29,13 @@ public class Regon extends Number {
                 tab[i] = Character.getNumericValue(super.getNumber().charAt(i));
             }
 
-            if (regonLength == 9) {
-                for (int i = 0; i < 8; i++) checksum += tab[i] * weightTable9[i];
-                checksum = checksum % 11;
-                if (checksum == 10) checksum = 0;
-                super.setCorrect(checksum == tab[8]);
-            } else if (regonLength == 14) {
-
-            } else super.setCorrect(false);
-
+            for (int i = 0; i < regonLength - 1; i++) {
+                if (regonLength == 9) checksum += tab[i] * weightTable9[i];
+                else checksum += tab[i] * weightTable14[i];
+            }
+            checksum = checksum % 11;
+            if (checksum == 10) checksum = 0;
+            super.setCorrect(checksum == tab[regonLength - 1]);
         }
         super.setValidated();
     }
